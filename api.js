ea39c1206445ae4243396931ef969c69d7d80db1
nat-wd <nathan.roi@disroot.org>
2024-11-28 10:16:29 +0100

    feat : \n\t-route recherche un customer\n\t-route recherche toutes les commandes pour un customer

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // URL MongoDB et base de données
 const mongoUrl = 'mongodb://localhost:27017'; 
-const dbName = 'td5';
+const dbName = 'tp2';
 
 app.use(express.json());
 
@@ -47,8 +47,81 @@ app.get('/product/:productID', async (req, res) => {
     }
 });
 
+
+// Route pour rechercher un utilisateur
+app.get('/customer/:CustomerID', async (req, res) => {
+    const CustomerID = req.params.CustomerID; 
+    if (typeof CustomerID !== 'string') {
+        return res.status(400).json({ error: 'Invalid costumerID. Must be a string' });
+    }
+
+    let client;
+    try {
+        client = new MongoClient(mongoUrl);
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        const db = client.db(dbName);
+        const collection = db.collection('customers');
+
+        // Recherche dans MongoDB
+        const customer = await collection.findOne({ CustomerID }); 
+        if (customer) {
+            res.json(customer);
+        } else {
+            res.status(404).json({ error: 'customer not found' });
+        }
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (client) {
+            await client.close();
+        }
+    }
+});
+
+
+// Route pour rechercher les commandes d'un utilisateur
+app.get('/customer_order/:CustomerID', async (req, res) => {
+    const CustomerID = req.params.CustomerID; 
+    if (typeof CustomerID !== 'string') {
+        return res.status(400).json({ error: 'Invalid costumerID. Must be a string' });
+    }
+
+    let client;
+    try {
+        client = new MongoClient(mongoUrl);
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        const db = client.db(dbName);
+
+        const orderCollection = db.collection('orders');
+
+        const pipeline = [];
+        pipeline.push({$match: { 'CustomerID': CustomerID}});
+
+        const aggregationResult = await orderCollection.aggregate(pipeline).toArray();
+
+        if (aggregationResult) {
+            res.json(aggregationResult);
+        } else {
+            res.status(404).json({ error: 'order not found' });
+        }
+        
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (client) {
+            await client.close();
+        }
+    }
+});
+
+
 // Lancement du serveur
 app.listen(port, () => {
     console.log(`API server is running on http://localhost:${port}`);
 });
-
