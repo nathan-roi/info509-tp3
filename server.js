aa4de154ca23fc12c5e1137fb266b257d72feb73
nat-wd <nathan.roi@disroot.org>
2024-11-29 18:04:21 +0100

    test de l'api en cours

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const port = 2000;
 
 // Importation de la librairie path
 const path = require('path');
--- www/app/page.js
+++ www/app/page.js
@@ -1,40 +1,42 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  // document.getElementById('fetch').addEventListener('click', async () => {
-  //   try {
-  //     const response = await fetch('http://localhost:3000/product/11');
-      
-  //     if (!response.ok) {
-  //       throw new Error('Network response was not ok');
-  //     }
+export default async function Home() {
+  // let data = await fetch('http://localhost:2000/product/11');
+  // let res = await data.json();
+  // console.log(res);
+  const [rs] = React.useState();
 
-  //     const data = await response.json();
+  async function handleClick(){
+    try {
+      const response = await fetch('http://localhost:2000/product/11');
       
-  //     console.log('Product data:', data);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-  //     // Afficher les données sur la page HTML
-  //     const productInfo = `
-  //       <p>Product ID: ${data.productID}</p>
-  //       <p>Name: ${data.productName}</p>
-  //       <p>Price: $${data.unitPrice}</p>
-  //     `;
-     
-  //   const dproduct = document.getElementById('product') ; 
-  //   dproduct.innerHTML=productInfo ; 
+      const data = await response.json();
+      
+      console.log('Product data:', data);
+
+      // Afficher les données sur la page HTML
+      rs = `
+        <p>Product ID: ${data.productID}</p>
+        <p>Name: ${data.productName}</p>
+        <p>Price: $${data.unitPrice}</p>
+      `;
         
-  //   } catch (error) {
-  //           console.error('Error fetching product:', error);
-  //       }
-  // });
+    } catch (error) {
+      console.error('Error fetching product:', error);
+    }
+  }
   return (
     <div className={styles.page}>
       {/* <img width="250px" src="node-mongo.png"> */}
       <h1>Product</h1>
-      <button id="fetch">Fetch Product 11</button>
+      <button onClick={handleClick}>Fetch Product 11</button>
 
-	    <div id="product"></div>
+	    <div id="product">{rs}</div>
     </div>
   );
 }
