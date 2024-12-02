53d680f23082deedb5cc46a2f65a192925ada791
nat-wd <nathan.roi@disroot.org>
2024-12-02 22:06:41 +0100

    feat : ébauche de recherche dans un tableau

--- www/app/allOrders.js
+++ www/app/allOrders.js
@@ -1,61 +1,51 @@
-"use client"; 
-
-import { useState } from "react";
+"use client";
+import { useState } from 'react';
 
 export default function AllOrder({ orders }) {
-  const [showModal, setShowModal] = useState(false); 
+  const [searchText, setSearchText] = useState('');
 
-  const toggleModal = () => {
-    setShowModal(prevState => !prevState);
-  };
-
+  const result = orders.filter((order) => order.CustomerID.toLowerCase().includes(searchText.toLowerCase()));
   return (
-    <>     
-      <button onClick={toggleModal}>
-        COMMANDES
-      </button>
-
-      {showModal && (
-        <div className="modal" onClick={toggleModal} style={{ cursor: 'pointer' }}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()} style={{ cursor: 'auto' }}>
-            <div className="orders">
-              <table>
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Client</th>
-                    <th>Ville</th>
-                    <th>Code postal</th>
-                    <th>Pays</th>
-                    <th>Date de commande</th>
-                    <th>Date d'expédition</th>
-                    <th>Quantité</th>
-                    <th>Frais de port</th>
-                    <th>Prix</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders.map((order) => (
-                    <tr key={order._id}>
-                      <td>{order.CustomerID}</td>
-                      <td>{order.orderName}</td>
-                      <td>{order.ShipCity}</td>
-                      <td>{order.ShipPostalCode}</td>
-                      <td>{order.ShipCountry}</td>
-                      <td>{order.OrderDate}</td>
-                      <td>{order.ShippedDate}</td>
-                      <td>{order.Quantity}</td>
-                      <td>{order.Freight}</td>
-                      <td>{order.UnitPrice}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+    <>
+      <div className="modal-content">
+        <h2>Liste des commandes</h2>
+        <input type="search"  placeholder="Search" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+        <button>Search</button>
+        <div className="orders">
+          <table>
+            <thead>
+            <tr>
+              <th>ID</th>
+              <th>Client</th>
+              <th>Ville</th>
+              <th>Code postal</th>
+              <th>Pays</th>
+              <th>Date de commande</th>
+              <th>Date d'expédition</th>
+              <th>Quantité</th>
+              <th>Frais de port</th>
+              <th>Prix</th>
+            </tr>
+            </thead>
+            <tbody>
+            {result.map((order) => (
+                <tr key={order._id}>
+                  <td>{order.OrderID}</td>
+                  <td>{order.CustomerID}</td>
+                  <td>{order.ShipCity}</td>
+                  <td>{order.ShipPostalCode}</td>
+                  <td>{order.ShipCountry}</td>
+                  <td>{order.OrderDate}</td>
+                  <td>{order.ShippedDate}</td>
+                  <td>{order.Quantity}</td>
+                  <td>{order.Freight}</td>
+                  <td>{order.UnitPrice}</td>
+                </tr>
+            ))}
+            </tbody>
+          </table>
         </div>
-      )}
-
+      </div>
     </>
   );
 }
